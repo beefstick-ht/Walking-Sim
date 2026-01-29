1a8101fe08f825528aab295f354ac3e2e0476d16
beefstick-ht <ctrickel@pratt.edu>
2026-01-29 14:54:26 -0500

    camera controller, basic player controller

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+using UnityEngine.Rendering;
+
+public class CameraController : MonoBehaviour
+    //demo for player-controller camera in first-person
+{
+    public float mouseSensitivity = 90;
+
+    private float xRotation;
+    
+    //Awake method runs befre start
+    private void Start()
+    {
+        xRotation = 0;
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+
+    public void Update()
+    {
+        //get mouse input
+        //Time.deltaTime is time between previous frame, Time.time is seconds elapsed
+        float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
+        float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
+
+
+        xRotation -= mouseY;
+        xRotation = Mathf.Clamp(xRotation, -90, 90);
+
+        //rotate player around y axis, camera around x axis
+        transform.parent.Rotate(Vector3.up * mouseX);
+        transform.localRotation = Quaternion.Euler(xRotation, 0, 0);
+    }
+}
